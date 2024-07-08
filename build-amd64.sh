b9dda9dfbed9c8f74cda769e19d584195001e49b
RNDr. Šimon Tóth <happy.cerberus@gmail.com>
2024-07-08 22:28:14 +0200

    Fixed naming.

--- build-amd64.sh
+++ build-amd64.sh
@@ -1,2 +1,2 @@
 TAG=20240708
-docker buildx build --no-cache --push --platform linux/amd64 --tag happycerberus/devenv-trunk:latest-amd64 --tag happycerberus/devenv-trunk:${TAG}-amd64 .
+docker buildx build --no-cache --push --platform linux/amd64 --tag happycerberus/devenv-stable:latest-amd64 --tag happycerberus/devenv-stable:${TAG}-amd64 .
